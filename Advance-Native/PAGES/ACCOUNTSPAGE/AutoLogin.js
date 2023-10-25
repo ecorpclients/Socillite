5abbc6e6515890e32c79ae86296d92fb7fb3be81
ecorpclients <144456256+ecorpclients@users.noreply.github.com>
2023-10-25 08:06:28 -0700

    Add files via upload
    Signed-off-by: ecorpclients <144456256+ecorpclients@users.noreply.github.com>

--- Advance-Native/PAGES/ACCOUNTSPAGE/AutoLogin.js
+++ Advance-Native/PAGES/ACCOUNTSPAGE/AutoLogin.js
@@ -1,34 +1,41 @@
 import { HOMEPAGE } from "../HOMEPAGE/HomePage.js";
 import { LOGINPAGE } from "./LoginPage.js";
 
-const AUTOLOGIN=(DIV)=>{
+const AUTOLOGIN = (DIV) => {
+    const STOREDEMAIL = localStorage.getItem('UserEmail');
 
-    const STOREDEMAIL=localStorage.getItem('UserEmail')
+    if (STOREDEMAIL) {
+        const URL = `https://demo.naweri.com/end-points/test-email.php?test_email=${STOREDEMAIL}`;
 
-    const URL=`https://demo.naweri.com/end-points/test-email.php?test_email=${STOREDEMAIL}`
+        fetch(URL)
+            .then(res => res.json())
+            .then(data => {
 
-    fetch(URL)
+                console.log(data)
 
-    .then(res => res.json())
+                if (data.Status === 'success') {
 
-    .then(data => {
+                    // User is authenticated, redirect to homepage
+                    
+                    HOMEPAGE(DIV);
 
-        if (data.status === 'sucess') {
-            
-            localStorage.removeItem('UserEmail');
+                } else {
 
-            LOGINPAGE(DIV);           
+                    localStorage.removeItem('UserEmail');
+                    // User not authenticated, redirect to login page
+                    LOGINPAGE(DIV);
+                }
+            })
+            .catch(error => {
+                console.error(error);
+                // Handle fetch error if needed
+                // For example, you can redirect to the login page here as well
+                LOGINPAGE(DIV);
+            });
+    } else {
+        // No stored email, redirect to login page
+        LOGINPAGE(DIV);
+    }
+};
 
-        } else {
-               
-            HOMEPAGE(DIV);
-
-        }
-
-    })
-
-    .catch(error => console.log(error))
-
-}
-
-export{AUTOLOGIN};
+export { AUTOLOGIN };
